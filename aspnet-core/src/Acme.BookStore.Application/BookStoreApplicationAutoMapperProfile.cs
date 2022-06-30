c0b7f5263f6723bd032a31132a10060ee9c1513e
Ezequiel Vera <gurmiax@gmail.com>
2022-06-29 22:52:57 -0300

    Version1

---
+++ aspnet-core/src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Acme.BookStore.Books;
+
+namespace Acme.BookStore;
+
+public class BookStoreApplicationAutoMapperProfile : Profile
+{
+    
+        public BookStoreApplicationAutoMapperProfile()
+        {
+            CreateMap<Book, BookDto>();
+            CreateMap<CreateUpdateBookDto, Book>();
+    }
+    
+}
