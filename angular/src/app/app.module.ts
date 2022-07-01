848e2b48f9061ad73da8d07d80145ac0d9bc32d5
Ezequiel Vera <gurmiax@gmail.com>
2022-07-01 08:53:21 -0300

    Version PostPrimeNg

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { APP_ROUTE_PROVIDER } from './route.provider';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   imports: [
@@ -29,6 +30,7 @@ import { APP_ROUTE_PROVIDER } from './route.provider';
     TenantManagementConfigModule.forRoot(),
     SettingManagementConfigModule.forRoot(),
     ThemeBasicModule.forRoot(),
+    ButtonModule,
   ],
   declarations: [AppComponent],
   providers: [APP_ROUTE_PROVIDER],
--- angular/src/app/prueba/prueba.module.ts
+++ angular/src/app/prueba/prueba.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { PruebaRoutingModule } from './prueba-routing.module';
 import { PruebaComponent } from './prueba.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [PruebaComponent],
-  imports: [SharedModule, PruebaRoutingModule],
+  imports: [SharedModule, PruebaRoutingModule, ButtonModule],
 })
 export class HomeModule {}
